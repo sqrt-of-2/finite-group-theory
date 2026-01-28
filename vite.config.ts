6f2cbb56e3174ae0d5c75c3c005e448355bf5d2d
sqrt-of-2 <240895760+sqrt-of-2@users.noreply.github.com>
2026-01-28 15:09:26 +0100

    Configure deployment and clean types

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/finite-group-theory/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
